76cdb06bb3bed93d567482e735236ba05c9d17a6
lussds <lukssds@gmail.com>
2024-08-06 20:43:37 -0300

    intent study

---
+++ intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.intent
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.intent.databinding.ActivityMainBinding
+import com.example.intent.ui.theme.IntentTheme
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonIr.setOnClickListener{
+            val i = Intent(this, ProfileActivity::class.java)
+            startActivity(i)
+        }
+
+
+    }
+}
+
